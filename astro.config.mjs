7f79a60db10ebb485e9d164ea21301fe74c2e69c
Manuel Thomsen <masotoma@hotmail.com>
2025-03-05 22:37:19 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,12 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-
+import react from "@astrojs/react";
 import icon from "astro-icon";
-
-import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  output:'server',
-  integrations: [mdx(), sitemap(), tailwind(), icon()],
-  adapter: netlify(),
+  site: 'https://manuelito.tech',
+  output:'static',
+  integrations: [mdx(), sitemap(), tailwind(), icon(), react()]
 });
